9d0376cda25de03a3e2f83a524ce9a65ebcd146c
vasiliyzaprudin <vasiliyzaprudin@gmail.com>
2025-08-22 08:15:45 +0500

    Устранена ошибка в pom.xlm. Точки стоятся по нажатию мыши

--- src/main/java/org/example/pointvisualizer/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.pointvisualizer {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires javafx.base;
-
-    opens org.example.pointvisualizer to javafx.fxml;
-    exports org.example.pointvisualizer;
-}
